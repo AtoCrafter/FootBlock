6c343d6bea6e76832039c93e9fd3f7ed19a2de7e
AtoCrafter <ato.crafter@gmail.com>
2015-01-09 13:54:43 +0900

    Add Recipe

--- src/main/scala/ato/footblock/FootBlock.scala
+++ src/main/scala/ato/footblock/FootBlock.scala
@@ -8,6 +8,8 @@ import cpw.mods.fml.common.Mod.EventHandler
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
 import cpw.mods.fml.common.registry.GameRegistry
 import cpw.mods.fml.common.{Mod, SidedProxy}
+import net.minecraft.init.Items
+import net.minecraft.item.ItemStack
 
 @Mod(modid = "FootBlock", modLanguage = "scala")
 object FootBlock {
@@ -26,5 +28,9 @@ object FootBlock {
     GameRegistry.registerItem(itemSupplier, "FootBlockSupplier")
     GameRegistry.registerBlock(blockFoot, "FootBlock")
     GameRegistry.registerTileEntity(classOf[TileEntityFoot], "CollapseBlock")
+    GameRegistry.addRecipe(new ItemStack(itemSupplier),
+      "FSF",
+      Character.valueOf('F'), Items.feather,
+      Character.valueOf('S'), Items.nether_star)
   }
 }
